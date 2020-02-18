f45de05fccdff80b5c51952f2f8bdf025e38b322
RabbitFeet <syx@rabbitfeet.net>
2020-02-18 16:55:54 +0800

    修改md样式

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 import './src/style/index.less'
 import './src/style/github-markdown.css'
-// require('./src/style/prismTheme/prism-coy.css')
-require('./src/style/prismTheme/prism-solarizedlight.css')
+require('./src/style/prismTheme/prism-tomorrow.css')
+// require('./src/style/prismTheme/prism-solarizedlight.css')
 // require('./src/style/prismTheme/prism-line-numbers.css')
