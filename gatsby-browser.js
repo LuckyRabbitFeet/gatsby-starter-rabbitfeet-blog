6e63d67ac18af1186c78c6b351ea2fce4bdb0969
RabbitFeet <syx@rabbitfeet.net>
2020-03-03 00:23:25 +0800

    修改markdown样式

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 import './src/style/index.less'
-import './src/style/github-markdown.css'
+import './src/style/theme/github-markdown.less'
 require('./src/style/prismTheme/prism-tomorrow.css')
 // require('./src/style/prismTheme/prism-solarizedlight.css')
 // require('./src/style/prismTheme/prism-line-numbers.css')
