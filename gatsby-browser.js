c391ee3eaaeeda38089373037f1163dd1be4a90b
RabbitFeet <syx@rabbitfeet.net>
2020-03-09 21:09:03 +0800

    修改代码高亮样式，并删除无用样式

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 import './src/style/index.less'
 import './src/style/theme/github-markdown.less'
-require('./src/style/prismTheme/prism-tomorrow.css')
-// require('./src/style/prismTheme/prism-solarizedlight.css')
-// require('./src/style/prismTheme/prism-line-numbers.css')
+import './src/style/prism/prism-theme.css'
+import './src/style/prism/prism-title.css'
+import './src/style/prism/prism-line-numbers.css'
